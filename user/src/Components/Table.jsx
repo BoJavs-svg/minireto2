d8bb02509eb30735d948ffd9a8425adec073514c
BoJavs <javiereric1309@gmail.com>
2023-04-24 11:04:58 -0600

    Deploy

--- user/src/Components/Table.jsx
+++ user/src/Components/Table.jsx
@@ -7,11 +7,11 @@ function Table() {
     const path = window.location.pathname;
     useEffect(() => {
         fetch("/sushi")
-            .then(res => res.json())
+            .then(res => console.log(res))
             .then((data) => setData(data))
             .catch((err) => console.log(err));
         }, []);
-        
+
     const handleSubmit = (event) => {
         event.preventDefault();
         axios.post('/addSushi/' + event.target.getAttribute("sushi_id")+ "/" + path)
