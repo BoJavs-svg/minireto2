4a815acb35a398a6ff95fbf6c428e6030964eac0
BoJavs <javiereric1309@gmail.com>
2023-04-25 07:44:01 -0600

    Show response of api

--- user/src/Components/Table.jsx
+++ user/src/Components/Table.jsx
@@ -7,7 +7,7 @@ function Table() {
     const path = window.location.pathname;
     useEffect(() => {
         fetch("/sushi")
-            .then(res => res.response)
+            .then(res => res.data)
             .then((data) => setData(data))
             .catch((err) => console.log(err));
         }, []);
